4d8276ee7cf1dfce88058a27b310c4380ec0cf12
Prakash S k <fly2prakashkumar@gmail.com>
2026-01-15 02:39:03 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/layout/main-nav.tsx
+++ src/components/layout/main-nav.tsx
@@ -68,9 +68,9 @@ export function MainNav({ isMobile = false }: { isMobile?: boolean }) {
       <NavigationMenuList>
          {navLinks.map((link) => (
             <NavigationMenuItem key={link.href + link.label}>
-                <Link href={link.href} passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>{link.label}</NavigationMenuLink>
-                </Link>
+                <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                  <Link href={link.href}>{link.label}</Link>
+                </NavigationMenuLink>
             </NavigationMenuItem>
         ))}
         <NavigationMenuItem>
@@ -95,13 +95,13 @@ export function MainNav({ isMobile = false }: { isMobile?: boolean }) {
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<typeof Link>,
-  React.ComponentPropsWithoutRef<typeof Link> & { title: string }
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a"> & { title: string }
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link
+        <a
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -113,7 +113,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </Link>
+        </a>
       </NavigationMenuLink>
     </li>
   )
