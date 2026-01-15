ddeaf9cb54657e908d61178dbceb6654410d5d9b
Prakash S k <fly2prakashkumar@gmail.com>
2026-01-15 02:44:06 +0000

    add one more "Handwash" (_for element div_)

--- src/components/layout/main-nav.tsx
+++ src/components/layout/main-nav.tsx
@@ -18,6 +18,7 @@ const navLinks = [
     { href: '/products', label: 'Both&Body' },
     { href: '/products', label: 'Skin care' },
     { href: '/products', label: 'Toothpaste' },
+    { href: '/products', label: 'Handwash' },
 ];
 
 export function MainNav({ isMobile = false }: { isMobile?: boolean }) {
@@ -53,13 +54,13 @@ export function MainNav({ isMobile = false }: { isMobile?: boolean }) {
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a"> & { title: string }
+  React.ElementRef<typeof Link>,
+  React.ComponentPropsWithoutRef<typeof Link> & { title: string }
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -71,7 +72,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   )
