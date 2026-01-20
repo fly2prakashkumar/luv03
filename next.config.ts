f11e670ab1afff9a6b458223fa4b095f895a0656
Prakash S k <fly2prakashkumar@gmail.com>
2026-01-20 06:42:51 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://aqualog

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'aqualogica.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
